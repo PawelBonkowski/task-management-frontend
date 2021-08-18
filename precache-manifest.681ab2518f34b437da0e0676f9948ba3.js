a68dd2891f4526b7f76564fb0c81d496855c5340
PawelBonkowski <PawelBonkowski@users.noreply.github.com>
2021-08-18 16:30:23 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.681ab2518f34b437da0e0676f9948ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695bf5d39740e176387a0a7fb2991ae2",
+    "url": "https://PawelBonkowski.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ec257acd2be309d9c1c1",
+    "url": "https://PawelBonkowski.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "eea79a9e57c2e03d4d83",
+    "url": "https://PawelBonkowski.github.io/task-management-frontend/static/js/2.c0d1c7a7.chunk.js"
+  },
+  {
+    "revision": "ec257acd2be309d9c1c1",
+    "url": "https://PawelBonkowski.github.io/task-management-frontend/static/js/main.de5f232b.chunk.js"
+  },
+  {
+    "revision": "c5a50998d52cd28a4c83",
+    "url": "https://PawelBonkowski.github.io/task-management-frontend/static/js/runtime~main.0262a78d.js"
+  }
+]);
